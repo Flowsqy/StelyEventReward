6d1338001d2913688ec1539ed75e8d26980b4be0
Flowsqy <Flo504.Dev@gmail.com>
2021-07-01 00:12:50 +0200

    Init project

---
+++ src/main/java/fr/flowsqy/stelyeventreward/StelyEventRewardPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.stelyeventreward;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StelyEventRewardPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
