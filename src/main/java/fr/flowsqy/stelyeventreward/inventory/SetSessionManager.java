da6eb2c22208cd1b01e935d84e413af7d69622b5
Flowsqy <Flo504.Dev@gmail.com>
2021-07-02 00:46:44 +0200

    Remove creative protection, too annoying and performance waste

--- src/main/java/fr/flowsqy/stelyeventreward/inventory/SetSessionManager.java
+++ src/main/java/fr/flowsqy/stelyeventreward/inventory/SetSessionManager.java
@@ -3,25 +3,13 @@ package fr.flowsqy.stelyeventreward.inventory;
 import fr.flowsqy.stelyeventreward.StelyEventRewardPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,15 +27,12 @@ public class SetSessionManager implements Listener {
     // Session manipulation
 
     public void open(Player player) {
-        final MoveProtectionTask moveProtectionTask = new MoveProtectionTask(player);
         sessions.put(player.getUniqueId(), new SetSession(
                 player.getInventory().getContents(),
-                player.getGameMode(),
-                moveProtectionTask)
+                player.getGameMode())
         );
         player.getInventory().clear();
         player.setGameMode(GameMode.CREATIVE);
-        moveProtectionTask.start();
     }
 
     public boolean close(Player player, Runnable closeCallback) {
@@ -80,92 +65,12 @@ public class SetSessionManager implements Listener {
         inventory.clear();
         inventory.setContents(session.originalContent());
         player.setGameMode(session.previousGameMode());
-        session.moveProtectionTask().cancel();
-    }
-
-    private boolean fastCheckPlayer(UUID uuid) {
-        return !sessions.isEmpty() && sessions.containsKey(uuid);
-    }
-
-    // Protection
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    private void onPlayerInteract(PlayerInteractEvent event) {
-        final Player player = event.getPlayer();
-        if (fastCheckPlayer(player.getUniqueId())) {
-            if (event.getMaterial() == Material.ANVIL) {
-                // Open an anvil inventory, but it's annoying
-            } else {
-                event.setCancelled(true);
-            }
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    private void onPlayerClickInventory(InventoryClickEvent event) {
-        if (event.getInventory().getType() == InventoryType.ENDER_CHEST && fastCheckPlayer(event.getWhoClicked().getUniqueId())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    private void onPlayerDragInventory(InventoryDragEvent event) {
-        if (event.getInventory().getType() == InventoryType.ENDER_CHEST && fastCheckPlayer(event.getWhoClicked().getUniqueId())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    private void onPlayerDropItem(PlayerDropItemEvent event) {
-        if (fastCheckPlayer(event.getPlayer().getUniqueId())) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    private void onPlayerPickupItem(EntityPickupItemEvent event) {
-        if (event.getEntityType() == EntityType.PLAYER && fastCheckPlayer(event.getEntity().getUniqueId())) {
-            event.setCancelled(true);
-        }
     }
 
     private final record SetSession(
             ItemStack[] originalContent,
-            GameMode previousGameMode,
-            MoveProtectionTask moveProtectionTask
+            GameMode previousGameMode
     ) {
-    }
-
-    private final static class MoveProtectionTask extends BukkitRunnable {
-
-        private final Player player;
-        private final Location originalLocation;
-
-        public MoveProtectionTask(Player player) {
-            this.player = player;
-            originalLocation = player.getLocation();
-        }
-
-        @Override
-        public void run() {
-            if (!compare(originalLocation, player.getLocation())) {
-                originalLocation.setYaw(player.getLocation().getYaw());
-                originalLocation.setPitch(player.getLocation().getPitch());
-                player.teleport(originalLocation, PlayerTeleportEvent.TeleportCause.UNKNOWN);
-            }
-        }
-
-        private boolean compare(Location first, Location second) {
-            return
-                    first.getBlockX() == second.getBlockX()
-                            && first.getBlockY() == second.getBlockY()
-                            && first.getBlockZ() == second.getBlockZ();
-        }
-
-        public void start() {
-            runTaskTimer(JavaPlugin.getPlugin(StelyEventRewardPlugin.class), 0L, 20L);
-        }
-
     }
 
 }
